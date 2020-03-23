b0eceadb9884333eaf3f0f11959093ad2e938e8e
tobias-fyi <tobyreaper@gmail.com>
2020-03-23 15:22:32 -0600

    First pass implementation of Queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,39 @@
+"""
+Data Structures :: Queue
+"""
+
 import sys
-sys.path.append('../doubly_linked_list')
+
+sys.path.append("../doubly_linked_list")
 from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
     def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # self.size = 0
+        # Use a DLL to store our elements because already
+        # set up with needed methods
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        """Adds an item to the back of the queue.
+
+        :param value : Item to be added to the queue.
+        """
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        """Removes an item from the front of the queue.
+        
+        :return value : Value of dequeued item or None.
+        """
+        # Empty queue case is handled by DLL method
+        value = self.storage.remove_from_head()
+        return value
 
     def len(self):
-        pass
+        """Calls `len()` on the queue.
+        
+        :return length (int) : Length of queue.
+        """
+        return len(self.storage)
