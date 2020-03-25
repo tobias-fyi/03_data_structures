2f4ad6e20b0bef0757259cac17a66bc5fc16ccd4
tobias-fyi <tobyreaper@gmail.com>
2020-03-25 12:07:00 -0600

    633 Lecture - binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../queue_and_stack')
+
+sys.path.append("../queue_and_stack")
 from dll_queue import Queue
 from dll_stack import Stack
 
@@ -19,9 +20,12 @@ class BinarySearchTree:
     def contains(self, target):
         pass
 
-    # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        """Return the maximum value found in the tree."""
+        if self.right:
+            self.right.get_max()
+        else:
+            return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
