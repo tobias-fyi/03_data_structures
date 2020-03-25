ff3d3d1b67869afcf4ac2faf5f4126b88b1ec1c5
tobias-fyi <tobyreaper@gmail.com>
2020-03-25 16:57:48 -0600

    Binary search day 1 MVP

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,9 @@
+"""
+Data Structures :: Binary search tree
+"""
+
+
+# %%
 import sys
 
 sys.path.append("../queue_and_stack")
@@ -5,57 +11,110 @@ from dll_queue import Queue
 from dll_stack import Stack
 
 
+# %%
 class BinarySearchTree:
     def __init__(self, value):
+        """Implementation of a binary search tree.
+        Each node can be considered a leaf and a complete tree.
+        """
         self.value = value
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
     def insert(self, value):
-        pass
+        """Inserts the given value into the tree."""
+        if value < self.value:
+            if self.left is None:  # Base case: empty leaf
+                self.left = BinarySearchTree(value)
+            else:  # Recursive case
+                self.left.insert(value)
+        else:
+            if self.right is None:  # Base case: empty leaf
+                self.right = BinarySearchTree(value)
+            else:  # If exists: recur into right
+                self.right.insert(value)
 
-    # Return True if the tree contains the value
-    # False if it does not
     def contains(self, target):
-        pass
+        """Searches the tree for the target value.
+        
+        :param target : Target value.
+        :return contains (Bool) : Indicates if target is in tree.
+        """
+        # Compare value to target
+        if self.value == target:
+            return True
+        # === Go left ===
+        if target < self.value:
+            if self.left is None:  # If left is None, return None (base case)
+                return False
+            else:  # If left exists, recur left
+                return self.left.contains(target)
+        # === Go right ===
+        else:
+            if self.right is None:  # If right is None, return None (base case)
+                return False
+            else:  # If right exists, recur right
+                return self.right.contains(target)
 
     def get_max(self):
         """Return the maximum value found in the tree."""
-        if self.right:
-            self.right.get_max()
-        else:
+        if self.right is None:  # Rightmost value is maximum
             return self.value
+        else:
+            return self.right.get_max()
 
-    # Call the function `cb` on the value of each node
-    # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        """Calls the function `cb()` on the value of each node.
+        You may use a recursive or iterative approach.
+        """
+        cb(self.value)  # Call function on current node's value
+        # === Go left === #
+        if self.left is not None:  # Left exists, recur left
+            self.left.for_each(cb)
+        # === Go right === #
+        if self.right is not None:  # Right exists, recur right
+            self.right.for_each(cb)
 
-    # DAY 2 Project -----------------------
+    # ====== DAY 2 Project ====== #
 
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
+        """Prints all the values in order from low to high.
+        Hint: Use a recursive, depth first traversal
+        """
         pass
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
     def bft_print(self, node):
+        """Prints the value of every node, starting with the given node,
+        in an iterative breadth first traversal
+        """
         pass
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
     def dft_print(self, node):
+        """Prints the value of every node, starting with the given node,
+        in an iterative depth first traversal.
+        """
         pass
 
-    # STRETCH Goals -------------------------
+    # ====== STRETCH Goals ====== #
     # Note: Research may be required
 
-    # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
+        """Prints Pre-order recursive DFT."""
         pass
 
-    # Print Post-order recursive DFT
     def post_order_dft(self, node):
+        """Prints Post-order recursive DFT."""
         pass
+
+
+# %%
+bst = BinarySearchTree(5)
+bst.insert(2)
+bst.insert(3)
+bst.insert(7)
+bst.insert(6)
+bst.insert(16)
+bst.insert(63)
+
+
+# %%
