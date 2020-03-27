96a034ed372ca442feaeda686d2d623156cb6429
tobias-fyi <tobyreaper@gmail.com>
2020-03-27 11:32:51 -0600

    Attempted further optimization

--- Sprint-Challenge--Data-Structures-Python/names/names.py
+++ Sprint-Challenge--Data-Structures-Python/names/names.py
@@ -1,3 +1,9 @@
+"""
+Data Structures :: Sprint Challenge
+
+Runtime optimization
+"""
+
 import time
 
 from binary_search_tree import BinarySearchTree
@@ -17,10 +23,14 @@ duplicates = []  # Return the list of duplicates in this data structure
 # Replace the nested for loops below with your improvements
 
 # First improvement is simple syntax change
+# ** This is also a potential solution to the Stretch Goal
+# ** as this implementation uses only lists.
 # for name_1 in names_1:
 #     if name_1 in names_2:
 #         duplicates.append(name_1)
 # >>> runtime: 1.5658197402954102 seconds
+# Although faster, the program still has to go through every
+# item in both lists. Therefore the complexity is still O(n^2).
 
 # Second method of improvement
 # Using a data structure from this week: Binary Search Tree
@@ -37,15 +47,26 @@ for name_2 in names_2:  # Loop through second list to search
 # Complexity is O(n + n) = O(2n) = O(n) for BST
 # as opposed to O(n * n) = O(n^2) for the nested loops
 
-# ---------- Stretch Goal -----------
+# ============ Stretch Goal ============ #
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
 
-# Third / stretch improvement
+# Third / Stretch improvement
 # Using Python set intersection
 # duplicates = list(set(names_1) & set(names_2))
 # >>> runtime: 0.004755973815917969 seconds
+# By far the most efficient out of all of my solutions
+
+# Fourth / Stretch 2
+# Using a Python generator
+# Create generator using generator expression
+# dupe_gen = (n for n in names_1 if n in names_2)
+# # Convert into a list
+# duplicates = list(dupe_gen)
+
+# >>> runtime: 1.586595058441162 seconds
+# Virtually identical to the first solution.
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
