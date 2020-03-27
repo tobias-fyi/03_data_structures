7ad090a3980aa24587d11c204401ae4a3a18f22b
tobias-fyi <tobyreaper@gmail.com>
2020-03-27 09:56:10 -0600

    First-pass RingBuffer implementation

--- Sprint-Challenge--Data-Structures-Python/ring_buffer/doubly_linked_list.py
+++ Sprint-Challenge--Data-Structures-Python/ring_buffer/doubly_linked_list.py
@@ -1,46 +1,47 @@
-"""Each ListNode holds a reference to its previous node
-as well as its next node in the List."""
+"""
+Data Structures :: Doubly-Linked List
+"""
 
 
 class ListNode:
     def __init__(self, value, prev=None, next=None):
+        """Each ListNode holds a reference to its previous node
+        as well as its next node in the List."""
         self.value = value
         self.prev = prev
         self.next = next
 
-    """Wrap the given value in a ListNode and insert it
-    after this node. Note that this node could already
-    have a next node it is point to."""
     def insert_after(self, value):
+        """Wrap the given value in a ListNode and insert it
+        after this node. Note that this node could already
+        have a next node it is point to."""
         current_next = self.next
         self.next = ListNode(value, self, current_next)
         if current_next:
             current_next.prev = self.next
 
-    """Wrap the given value in a ListNode and insert it
-    before this node. Note that this node could already
-    have a previous node it is point to."""
     def insert_before(self, value):
+        """Wrap the given value in a ListNode and insert it
+        before this node. Note that this node could already
+        have a previous node it is point to."""
         current_prev = self.prev
         self.prev = ListNode(value, current_prev, self)
         if current_prev:
             current_prev.next = self.prev
 
-    """Rearranges this ListNode's previous and next pointers
-    accordingly, effectively deleting this ListNode."""
     def delete(self):
+        """Rearranges this ListNode's previous and next pointers
+        accordingly, effectively deleting this ListNode."""
         if self.prev:
             self.prev.next = self.next
         if self.next:
             self.next.prev = self.prev
 
 
-"""Our doubly-linked list class. It holds references to
-the list's head and tail nodes."""
-
-
 class DoublyLinkedList:
     def __init__(self, node=None):
+        """Our doubly-linked list class. It holds references to
+        the list's head and tail nodes."""
         self.head = node
         self.tail = node
         self.length = 1 if node is not None else 0
@@ -48,10 +49,10 @@ class DoublyLinkedList:
     def __len__(self):
         return self.length
 
-    """Wraps the given value in a ListNode and inserts it 
-    as the new head of the list. Don't forget to handle 
-    the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
+        """Wraps the given value in a ListNode and inserts it 
+        as the new head of the list. Don't forget to handle 
+        the old head node's previous pointer accordingly."""
         new_node = ListNode(value, None, None)
         self.length += 1
         if not self.head and not self.tail:
@@ -62,18 +63,18 @@ class DoublyLinkedList:
             self.head.prev = new_node
             self.head = new_node
 
-    """Removes the List's current head node, making the
-    current head's next node the new head of the List.
-    Returns the value of the removed Node."""
     def remove_from_head(self):
+        """Removes the List's current head node, making the
+        current head's next node the new head of the List.
+        Returns the value of the removed Node."""
         value = self.head.value
         self.delete(self.head)
         return value
 
-    """Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
-    the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
+        """Wraps the given value in a ListNode and inserts it 
+        as the new tail of the list. Don't forget to handle 
+        the old tail node's next pointer accordingly."""
         new_node = ListNode(value, None, None)
         self.length += 1
         if not self.head and not self.tail:
@@ -84,35 +85,35 @@ class DoublyLinkedList:
             self.tail.next = new_node
             self.tail = new_node
 
-    """Removes the List's current tail node, making the 
-    current tail's previous node the new tail of the List.
-    Returns the value of the removed Node."""
     def remove_from_tail(self):
+        """Removes the List's current tail node, making the 
+        current tail's previous node the new tail of the List.
+        Returns the value of the removed Node."""
         value = self.tail.value
         self.delete(self.tail)
         return value
 
-    """Removes the input node from its current spot in the 
-    List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
+        """Removes the input node from its current spot in the 
+        List and inserts it as the new head node of the List."""
         if node is self.head:
             return
         value = node.value
         self.delete(node)
         self.add_to_head(value)
 
-    """Removes the input node from its current spot in the 
-    List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
+        """Removes the input node from its current spot in the 
+        List and inserts it as the new tail node of the List."""
         if node is self.tail:
             return
         value = node.value
         self.delete(node)
         self.add_to_tail(value)
 
-    """Removes a node from the list and handles cases where
-    the node was the head or the tail"""
     def delete(self, node):
+        """Removes a node from the list and handles cases where
+        the node was the head or the tail"""
         self.length -= 1
         if self.head is self.tail:
             self.head = None
@@ -126,8 +127,8 @@ class DoublyLinkedList:
         else:
             node.delete()
 
-    """Returns the highest value currently in the list"""
     def get_max(self):
+        """Returns the highest value currently in the list"""
         max_value = self.head.value
         current = self.head
         while current is not None:
--- Sprint-Challenge--Data-Structures-Python/ring_buffer/ring_buffer.py
+++ Sprint-Challenge--Data-Structures-Python/ring_buffer/ring_buffer.py
@@ -1,26 +1,54 @@
 from doubly_linked_list import DoublyLinkedList
+from typing import Any
 
 
 class RingBuffer:
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
 
-    def append(self, item):
-        pass
+    def append(self, item: Any) -> None:
+        """Adds an element to the buffer."""
+        if len(self.storage) < self.capacity:
+            # If buffer is not full, simply add to tail
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail  # Update cursor
+        else:  # If buffer is full, overwrite next item
+            # If cursor is on tail, loop back to head
+            if self.current.next is None:
+                # Replace head node with new item
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current = self.storage.head  # Set cursor to new head
+            else:  # If cursor is not at tail, replace next item
+                # Uses the ListNode methods (don't update list length)
+                self.current.next.delete()  # Remove next item
+                # Insert new item after current
+                self.current.insert_after(item)
+                # Set current to next item
+                self.current = self.current.next
 
     def get(self):
+        """Returns all of the elements in the buffer 
+        in a list in their given order.
+        """
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        # Set initial current node
+        node = self.storage.head
+
+        # Iterate through DLL, appending values to list in order
+        while node:
+            if node.value is not None:
+                list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
 
+
 # ----------------Stretch Goal-------------------
-
-
 class ArrayRingBuffer:
     def __init__(self, capacity):
         pass
